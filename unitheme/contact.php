74708cfd7cc6628e3f37d18707f4ff4739dcc1bf
freemobil <108952994+freemobil@users.noreply.github.com>
2022-07-26 04:41:45 +0300

    git

---
+++ unitheme/contact.php
@@ -0,0 +1,97 @@
+<?php 
+
+	/* ==========================  Define variables ========================== */
+
+	#Your e-mail address
+	define("__TO__", "a.bublaku@gmail.com");
+
+	#Message subject
+	define("__SUBJECT__", "");
+
+	#Success message
+	define('__SUCCESS_MESSAGE__', "Your message has been sent. Thank you!");
+
+	#Error message 
+	define('__ERROR_MESSAGE__', "Error, your message hasn't been sent");
+
+	#Messege when one or more fields are empty
+	define('__MESSAGE_EMPTY_FILDS__', "Please fill out  all fields");
+
+	/* ========================  End Define variables ======================== */
+
+	//Send mail function
+	function send_mail($to,$subject,$message,$headers){
+		if(@mail($to,$subject,$message,$headers)){
+			echo json_encode(array('info' => 'success', 'msg' => __SUCCESS_MESSAGE__));
+		} else {
+			echo json_encode(array('info' => 'error', 'msg' => __ERROR_MESSAGE__));
+		}
+	}
+
+	//Check e-mail validation
+	function check_email($email){
+		if(!@eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	//Get post data
+	if(isset($_POST['name']) and isset($_POST['mail']) and isset($_POST['comment'])){
+		$name 	 = $_POST['name'];
+		$mail 	 = $_POST['mail'];
+		$website  = $_POST['website'];
+		$comment = $_POST['comment'];
+
+		if($name == '') {
+			echo json_encode(array('info' => 'error', 'msg' => "Please enter your name."));
+			exit();
+		} else if($mail == '' or check_email($mail) == false){
+			echo json_encode(array('info' => 'error', 'msg' => "Please enter valid e-mail."));
+			exit();
+		} else if($comment == ''){
+			echo json_encode(array('info' => 'error', 'msg' => "Please enter your message."));
+			exit();
+		} else {
+			//Send Mail
+			$to = __TO__;
+			$subject = __SUBJECT__ . ' ' . $name;
+			$message = '
+			<html>
+			<head>
+			  <title>Mail from '. $name .'</title>
+			</head>
+			<body>
+			  <table class="table">
+				<tr>
+				  <th align="right">Name:</th>
+				  <td align="left">'. $name .'</td>
+				</tr>
+				<tr>
+				  <th align="right">E-mail:</th>
+				  <td align="left">'. $mail .'</td>
+				</tr>
+				<tr>
+				  <th align="right">subject:</th>
+				  <td align="left">'. $subject .'</td>
+				</tr>
+				<tr>
+				  <th align="right">Comment:</th>
+				  <td align="left">'. $comment .'</td>
+				</tr>
+			  </table>
+			</body>
+			</html>
+			';
+
+			$headers  = 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+			$headers .= 'From: ' . $mail . "\r\n";
+
+			send_mail($to,$subject,$message,$headers);
+		}
+	} else {
+		echo json_encode(array('info' => 'error', 'msg' => __MESSAGE_EMPTY_FILDS__));
+	}
+ ?>
---
+++ videoheader/js/script.js
@@ -0,0 +1,113 @@
+var HeaderVideo = (function ($, document) {
+    
+    var settings = {
+        container: $('.header-video'),
+        header: $('.header-video--media'),
+        videoTrigger: $("#video-trigger"),
+        autoPlayVideo: false
+    }
+
+    var init = function(options){
+        settings = $.extend(settings, options);
+        getVideoDetails();
+        setFluidContainer();
+        bindClickAction();
+        
+        if(videoDetails.teaser) {
+            appendTeaserVideo();
+        }
+
+        if(settings.autoPlayVideo) {
+            appendFrame();
+        }
+    }
+
+    var getVideoDetails = function() {
+        videoDetails = {
+            id: settings.header.attr('data-video-src'),
+            teaser: settings.header.attr('data-teaser-source'),
+            provider: settings.header.attr('data-provider').toLowerCase(),
+            videoHeight: settings.header.attr('data-video-height'),
+            videoWidth: settings.header.attr('data-video-width')
+        }
+        return videoDetails;
+    };
+
+    var setFluidContainer = function () {
+
+        settings.container.data('aspectRatio', videoDetails.videoHeight / videoDetails.videoWidth);
+
+        $(window).resize(function(){
+            var winWidth = $(window).width(),
+                winHeight = $(window).height();
+
+            settings.container
+                .width(winWidth)
+                .height(winWidth * settings.container.data('aspectRatio'));
+
+            if(winHeight < settings.container.height()) {
+                settings.container
+                    .width(winWidth)
+                    .height(winHeight);
+            }
+
+        }).trigger('resize');
+
+    };
+
+    var bindClickAction = function() {
+        settings.videoTrigger.on("click", function(e) {
+            e.preventDefault();
+            appendFrame();
+        });
+    };
+
+    var appendTeaserVideo = function() {
+        if(Modernizr.video && !isMobile()) {
+            var source = videoDetails.teaser,
+                html = '<video autoplay="true" loop="loop" muted id="teaser-video" class="teaser-video"><source src="'+source+'.mp4" type="video/mp4"><source src="'+source+'.ogv" type="video/ogg"></video>';
+            settings.container.append(html);
+        }
+    };
+    
+    var createFrame = function() {
+        if(videoDetails.provider === 'youtube') {
+            var html = '<iframe src="//www.youtube.com/embed/'+videoDetails.id+'?rel=0&amp;hd=1&autohide=1&showinfo=0&autoplay=1&enablejsapi=1&origin=*" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        }
+        else if(videoDetails.provider === 'vimeo') {
+            var html = '<iframe src="//player.vimeo.com/video/'+videoDetails.id+'?title=0&amp;byline=0&amp;portrait=0&amp;color=3d96d2&autoplay=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        }
+        else if(videoDetails.provider === 'html5') {
+            var html = '<video autoplay="true" loop="loop" id="video"><source src="'+videoDetails.id+'.mp4" type="video/mp4"><source src="'+videoDetails.id+'.ogv" type="video/ogg"></video>';
+        }
+        return html;
+    };
+
+    var appendFrame = function() {
+        settings.header.hide();
+        settings.container.append(createFrame());
+        removePlayButton();
+        $('#teaser-video').hide();
+    };
+
+    var removePlayButton = function () {
+        if(settings.videoTrigger) {
+            settings.videoTrigger.fadeOut('slow');
+        }
+    };
+
+    var isMobile = function () {
+        if($(window).width() < 900 && Modernizr.touch) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    return {
+        init: init
+    };
+    
+})(jQuery, document);
