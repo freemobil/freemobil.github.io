05df00e0aa2766779763ec00b486b68b813637de
freemobil <108952994+freemobil@users.noreply.github.com>
2022-10-19 04:52:17 +0300

    html

--- Springfield/js/NProgress-start.js
+++
@@ -1,9 +0,0 @@
-$('body').show();
-NProgress.start();
-
-$(window).load(function() {
-  // Animate loader off screen
-  NProgress.done();
-  setTimeout(function() { $('.hider').fadeOut(); }, 600);
-});
-$('.blank').addClass('hider');
